536d6cee56199b6d11068b70967754f653d682e3
Prabjot Singh <125903944+singh1733@users.noreply.github.com>
2024-05-11 23:45:58 -0400

    Modularized code by making a function to produce the grid, and allowed users to click a button to choose size of grid

--- main.js
+++ main.js
@@ -1,24 +1,47 @@
-const divcontainer=document.querySelector("div.container");
+const divcontainer = document.querySelector("div.container");
 divcontainer.setAttribute("style", "display:flex; flex-direction: column; margin: 0; padding:0;");
 
-const baseSize = 'min(100vw, 100vh)'; // Use the smaller of the viewport width or height
+const baseSize = 'min(75vw, 75vh)'; // Use the smaller of the viewport width or height
 divcontainer.style.width = baseSize;
 divcontainer.style.height = baseSize;
 
-document.body.setAttribute("style", "display:flex; justify-content:center; min-height: 100vh;margin: 0px; padding: 0px;");
+let gridDim = 16;
+gridProducer();
+const button = document.createElement("button");
+button.textContent = "Set Size";
+button.addEventListener("click", () => {
+    gridDim = -1;
+    while (gridDim < 0 || gridDim > 100) {
+        gridDim = prompt("Enter a number 1-100 for dimensions of the grid");
+    }
+    gridProducer();
+});
 
-for (let j=0;j<16;j++){
-const divHolder=document.createElement("div");
-divHolder.classList.add("row");
-divHolder.setAttribute("style", "height: calc(100% / 16); width: 100%; display: flex;");
-for (let i=0;i<16;i++){
-    const div=document.createElement("div");
-    div.classList.add("single");
-    div.setAttribute("style", "border-style: solid;aspect-ratio: 1/1;flex: 1;");
-    div.addEventListener("mouseenter", ()=>{
-        div.style.backgroundColor="red";
-    });
-    divHolder.appendChild(div);
+document.body.insertBefore(button, divcontainer);
+
+document.body.setAttribute("style", "display:flex; align-items:center; min-height: 100vh;margin: 0px; padding: 0px; flex-direction:column;justify-content:center; background-color: #9EA1D4;");
+
+
+
+function gridProducer() {
+    divcontainer.innerHTML = "";
+    for (let j = 0; j < gridDim; j++) {
+
+        const divHolder = document.createElement("div");
+        divHolder.classList.add("row");
+        divHolder.setAttribute("style", "height: calc(100% /" + gridDim + "); width: 100%; display: flex;");
+        for (let i = 0; i < gridDim; i++) {
+            const div = document.createElement("div");
+            div.classList.add("single");
+            div.setAttribute("style", "border-style: solid;aspect-ratio: 1/1;flex: 1;");
+            div.style.backgroundColor = "white";
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = "rgb(";
+            });
+            divHolder.appendChild(div);
+        }
+        divcontainer.appendChild(divHolder);
+    }
 }
-divcontainer.appendChild(divHolder);
-}
+
+
